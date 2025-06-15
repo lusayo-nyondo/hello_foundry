d618e968e3bf340c03b33366f09dd6cddaa39790
lusayo-nyondo <lusayohendy@gmail.com>
2025-06-15 23:14:20 +0200

    Created payable contract.

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.13;
 contract Counter {
     uint256 public number;
 
+    event Log(string mesage, uint256 value);
+
     function setNumber(uint256 newNumber) public {
         number = newNumber;
+        emit Log("Set number to: ", newNumber);
     }
 
     function increment() public {
